b6f5d927e940e66c35d2c6b4f3d58d42bd9d2db0
Monika <moncobra@comcast.net>
2023-01-24 12:55:45 -0800

    updated 404 img

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function NotFound() {
-  return <img src="https://media.tenor.com/I8UCLFI1ylgAAAAd/dino-caminando.gif" />;
+  return <img src="https://i.pinimg.com/originals/31/10/a4/3110a47adee324ed324fe453dab81122.gif" />;
 }
